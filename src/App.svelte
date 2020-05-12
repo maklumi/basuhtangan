61e79610b1a8a94effb00a36f27deb5a2ce3794b
maklumi <maklumi@yahoo.com>
2020-05-12 22:46:33 +0800

    commiting html

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,22 @@
-<h1>Percubaan</h1>
+<script>
+  import Timer from "./Timer.svelte";
+  import Howto from "./Howto.svelte";
+</script>
+
+<style>
+  h1,
+  h3 {
+    text-align: center;
+  }
+</style>
+
+<h1>Basuh Tangan</h1>
+
+<Timer />
+
+<Howto />
+
+<h3>
+  <a href="https://www.who.int/gpsc/clean_hands_protection/en/">Punca gambar</a>
+  <a href="fr">Punca bunyi</a>
+</h3>
